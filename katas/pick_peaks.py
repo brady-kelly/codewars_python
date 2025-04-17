0706c641daac604673c01c0b72fa6ff79ade6619
Brady Kelly <bradyk@staycontactable.com>
2025-04-17 13:19:33 +0200

    New kata

--- katas/pick_peaks.py
+++ katas/pick_peaks.py
@@ -1,2 +1,16 @@
+# [3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]
 def pick_peaks(arr):
-    pass
+    if len(arr) == 0:
+        return {"pos": [], "peaks": []}
+
+    pos = []
+    peaks = []
+
+    for i in range(1, len(arr) - 1):
+        if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
+            pos.append(i)
+
+    for i in range(1, len(pos)):
+        peaks.append(arr[pos])
+
+    return {"pos": pos, "peaks": peaks}
