725caa65c8ac642a37d7cb5348d5142637b80b8e
Brady Kelly <bradyk@staycontactable.com>
2025-04-20 20:01:45 +0200

    to_roman is working

--- katas/roman_numerals_helper.py
+++ katas/roman_numerals_helper.py
@@ -1,8 +1,34 @@
 class RomanNumerals:
+
     @staticmethod
     def to_roman(val : int) -> str:
-        return ''
+        roman = ''
+        wip = val
+        for dec in RomanNumerals.romans:
+            c = wip // dec
+            roman += RomanNumerals.romans[dec] * c
+            wip = wip % dec
+        return roman
 
     @staticmethod
     def from_roman(roman_num : str) -> int:
         return 0
+
+    romans = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I"
+    }
+
+if __name__ == '__main__':
+    RomanNumerals.to_roman(1990)
